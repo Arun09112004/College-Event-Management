81e3a02dd146e9d374642da919a9800b4346cca3
Arun09112004 <arun9112004@gmail.com>
2026-06-28 09:30:05 +0530

    update project

--- events/views.py
+++ events/views.py
@@ -115,3 +115,78 @@ def check_status(request):
 
     return render(request,'status.html',
                   {'status':status})
+
+def cricket(request):
+    return render(request, 'cricket.html')
+
+def football(request):
+    return render(request, 'football.html') 
+
+def volleyball(request):
+    return render(request, 'volleyball.html')                 
+
+def kabaddi(request):
+    return render(request, 'kabaddi.html')
+
+def athletics(request):
+    return render(request, 'athletics.html')
+
+def chess(request):
+    return render(request, 'chess.html')
+
+def badminton(request):
+    return render(request, 'badminton.html') 
+
+def solo_dance(request):
+    return render(request, 'solo_dance.html')   
+
+def group_dance(request):
+    return render(request, 'group_dance.html')   
+
+def singing(request):
+    return render(request, 'singing.html')
+
+def mime(request):
+    return render(request, 'mime.html')    
+
+def photography(request):
+    return render(request, 'photography.html')       
+
+def short_film(request):
+    return render(request, 'short_film.html')  
+
+def drama(request):
+    return render(request, 'drama.html')  
+
+from .models import Registration
+
+from .models import Registration
+
+def dashboard(request):
+
+    total = Registration.objects.count()
+    approved = Registration.objects.filter(status="Approved").count()
+    pending = Registration.objects.filter(status="Pending").count()
+    rejected = Registration.objects.filter(status="Rejected").count()
+
+    sports = Registration.objects.filter(department="Sports").count()
+    symposium = Registration.objects.filter(department="Computer Science").count() + \
+                 Registration.objects.filter(department="Commerce").count() + \
+                 Registration.objects.filter(department="Mathematics").count() + \
+                 Registration.objects.filter(department="Physics").count()
+
+    cultural = Registration.objects.filter(department="Cultural").count()
+    college_day = Registration.objects.filter(department="College Day").count()
+
+    context = {
+        'total': total,
+        'approved': approved,
+        'pending': pending,
+        'rejected': rejected,
+        'sports': sports,
+        'symposium': symposium,
+        'cultural': cultural,
+        'college_day': college_day,
+    }
+
+    return render(request, 'dashboard.html', context)
